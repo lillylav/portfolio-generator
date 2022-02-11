dce58619857ddd164c174976a696971cf1d1c9f4
lillyleiran <lilly.leiran@gmail.com>
2022-02-10 21:19:09 -0700

    create html using node

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
+const fs = require('fs');
+const generatePage = require('./src/page-template');
+
 const profileDataArgs = process.argv.slice(2);
 
-const generatePage = (userName, githubName) => `Name: ${userName}, Github: ${githubName}`;
+const [name, github] = profileDataArgs;
+
+fs.writeFile('./index.html', generatePage(name, github), err => {
+    if(err) throw new Error(err);
+
+    console.log('Portfolio Complete! Checkout index.html to see the output!');
+});
